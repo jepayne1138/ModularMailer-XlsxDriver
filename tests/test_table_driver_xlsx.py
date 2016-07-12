3ee56b95d1f335055676ba09414c41cb79b65564
jepayne1138 <jepayne1138@gmail.com>
2016-07-12 10:25:48 -0500

    Refactor the package to change the name to just xlsx_driver.
     - Changed applicable imports to use the new name.
     - Rewrote the requirements.txt file as it was encoded improperly,
         which was breaking 'pip install -r requirements.txt'

--- tests/test_table_driver_xlsx.py
+++ tests/test_table_driver_xlsx.py
@@ -1,12 +1,12 @@
 import unittest
 import unittest.mock as mock
-from table_driver_xlsx import table_driver_xlsx
+from xlsx_driver import xlsx_driver
 
 
 class MockTest(unittest.TestCase):
 
     @mock.patch(
-        'table_driver_xlsx.table_driver_xlsx.openpyxl.Workbook.active',
+        'xlsx_driver.xlsx_driver.openpyxl.Workbook.active',
         new_callable=mock.PropertyMock)
     def test__get_sheet_from_workbook_no_sheet_index(self, mock_active):
         """The active property is called if no sheet index provided.
@@ -22,15 +22,15 @@ class MockTest(unittest.TestCase):
         I think it is most likely that I will have to access the 'active'
         property to do this.
         """
-        wb = table_driver_xlsx.openpyxl.Workbook()
+        wb = xlsx_driver.openpyxl.Workbook()
         # A hacky attempt to best emulate opening as read-only
         wb.__read_only = True
         # Test the function and assert that active property was accessed
-        table_driver_xlsx._get_sheet_from_workbook(wb)
+        xlsx_driver._get_sheet_from_workbook(wb)
         mock_active.assert_called_once_with()
 
     @mock.patch(
-        'table_driver_xlsx.table_driver_xlsx.openpyxl.Workbook.worksheets',
+        'xlsx_driver.xlsx_driver.openpyxl.Workbook.worksheets',
         new_callable=mock.PropertyMock)
     def test__get_sheet_from_workbook_given_sheet_index(self, mock_worksheets):
         """Given a sheet index, select from the list of worksheets.
@@ -41,9 +41,9 @@ class MockTest(unittest.TestCase):
         refactoring, it should be safe to acknowledge the the worksheets()
         property should always be accessed over using the active() property.
         """
-        wb = table_driver_xlsx.openpyxl.Workbook()
+        wb = xlsx_driver.openpyxl.Workbook()
         # A hacky attempt to best emulate opening as read-only
         wb.__read_only = True
         # Test the function and assert that worksheets property was accessed
-        table_driver_xlsx._get_sheet_from_workbook(wb, sheet_index=0)
+        xlsx_driver._get_sheet_from_workbook(wb, sheet_index=0)
         mock_worksheets.assert_called_once_with()
