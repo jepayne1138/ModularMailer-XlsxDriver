f0bc2e1010f81e4385d7853ef0f77fa335c05cdd
jepayne1138 <jepayne1138@gmail.com>
2016-07-12 11:00:46 -0500

    Fill in setup.py with accurate metadata and add README and LICENSE

---
+++ README.rst
@@ -0,0 +1,14 @@
+ModularMailer-XlsxDriver
+========================
+
+Description
+-----------
+A plugin that allows an Excel \*.xlsx file to be imported into the Modular Mailer application. (For Excel97 \*.xls files, used the XlsDriver plugin)
+
+Installation
+------------
+Installation instructions will be updated as the Modular Mailer project takes shape. Currently, the plan is to distribute the plugins on PyPI, therefore installation will theoretically be the following::
+
+    pip install ModularMailer-XlsxDriver
+
+Installation requires the ModularMailer project to already be installed.
--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup
 
 setup(
-    name='TableDriverXlsX',
-    packages=['table_driver_xlsx'],
+    name='ModularMailer-XlsxDriver',
+    packages=['xlsx_driver'],
     version='0.0a1',
-    description='Plugin intended for a future project that imports .xlsx files',
+    description='ModularMailer plugin for interacting with xlsx files',
     author='James Payne',
     author_email='jepayne1138@gmail.com',
-    url='https://github.com/jepayne1138/<repo_name>',
+    url='https://github.com/jepayne1138/ModularMailer-XlsxDriver',
     license='BSD-new',
-    download_url='https://github.com/jepayne1138/<repo_name>/tarball/1.0a1',
-    keywords='plugin',
-    install_requires=['openpyxl', '<project the pugin is for>'],
-    classifiers=[],
+    download_url='https://github.com/jepayne1138/ModularMailer-XlsxDriver/tarball/0.0a1',
+    keywords='plugin xlsx',
+    install_requires=['openpyxl', 'ModularMailer'],
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: Plugins',
+        'License :: OSI Approved :: BSD License',
+    ],
 )
